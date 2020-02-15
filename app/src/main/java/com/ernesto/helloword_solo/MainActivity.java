e45bcf2d18baacfeb090589ad76199218b2b1145
Ernesto <vcpep02@gmail.com>
2020-02-14 21:10:10 -0300

    editado

---
+++ app/src/main/java/com/ernesto/helloword_solo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ernesto.helloword_solo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//qualquer coisa
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
